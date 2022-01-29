f319367d8dd7893a48e9d84318601cb3f7d4008a
Toast <shane.trewartha@gmail.com>
2022-01-29 21:37:09 +1100

    all block out scene stuff

---
+++ MutuallyAssuredConstruction/Assets/Scripts/Shane/FaceBlockForward.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceBlockForward : MonoBehaviour
+{
+    void Update()
+    {
+        Quaternion parentRotation = transform.parent.parent.rotation;
+        Quaternion rotationCorrection = Quaternion.Euler(parentRotation.eulerAngles.x, parentRotation.eulerAngles.y * -(1/45), parentRotation.eulerAngles.z);
+
+        transform.SetPositionAndRotation(transform.position, rotationCorrection);
+        //transform.Rotate(new Vector3(rotationCorrection.eulerAngles.x, rotationCorrection.eulerAngles.y * -1, rotationCorrection.eulerAngles.z));
+    }
+}
